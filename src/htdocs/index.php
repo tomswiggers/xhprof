0966663fb0323f4446c91155f473620256552009
Tom Swiggers <swiggers.tom@gmail.com>
2019-02-23 14:39:57 +0100

    specify run dir

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -80,7 +80,7 @@ $vbbar = ' class="vbbar"';
 $vrbar = ' class="vrbar"';
 $vgbar = ' class="vgbar"';
 
-$xhprof_runs_impl = new XHProfRuns_Default();
+$xhprof_runs_impl = new XHProfRuns_Default('runs');
 
 displayXHProfReport($xhprof_runs_impl, $params, $source, $run, $wts,
                     $symbol, $sort, $run1, $run2);
